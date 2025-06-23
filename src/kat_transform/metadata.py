cd4676450b3f94ea04a0d9c072b84b974868dac2
Kuyugama <mail.kuyugama@gmail.com>
2025-06-23 06:57:33 +0300

    Add composition of metadata via "|" operator

--- src/kat_transform/metadata.py
+++ src/kat_transform/metadata.py
@@ -1,4 +1,33 @@
-class FieldMetadata:
+import typing
+
+T = typing.TypeVar("T", bound="Metadata")
+
+
+class Metadata:
+    def __init__(self) -> None:
+        self.entries: tuple[Metadata, ...] = (self,)
+
+    def get_metadata(self, metadata_type: type[T], exact: bool = False) -> T | None:
+        for entry in self.entries:
+            if type(entry) is metadata_type:
+                return entry
+
+            if not exact and isinstance(entry, metadata_type):
+                return entry
+
+        return None
+
+    def __or__(self, other: "Metadata") -> "CombinedMetadata":
+        return CombinedMetadata(*self.entries, *other.entries)
+
+
+class CombinedMetadata(Metadata):
+    def __init__(self, *entries: Metadata):
+        super().__init__()
+        self.entries: tuple[Metadata, ...] = entries
+
+
+class FieldMetadata(Metadata):
     """
     Field metadata that can be used by other tools (like json schema generation)
 
@@ -6,7 +35,7 @@ class FieldMetadata:
     """
 
 
-class SchemaMetadata:
+class SchemaMetadata(Metadata):
     """
     Schema metadata that can be used by other tools (like json schema generation)
 
